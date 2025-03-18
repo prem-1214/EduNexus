f7babdca56513f0c2c4b57659b9ab98a7bd93e1a
Prem-Rajput <rajputpremkumar22@gmail.com>
2025-03-19 01:29:46 +0530

    remove sensitive information

--- Backend/db/conn.js
+++ Backend/db/conn.js
@@ -1,14 +1,9 @@
 import mongoose from "mongoose"
-// import app from "../app.js"
 
 const connectDB = async () =>{
     const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${process.env.DB_NAME}`)
-    // const connectionInstance = await mongoose.connect("mongodb+srv://EduNexus:EduNexus@edunexus.9vwq7.mongodb.net/EduNexus")
     .then(() =>{
     console.log("Connected to Mongodb")
-    // app.on("error", (error) =>{
-    //     console.log("Error during connecting to database", error)
-    // })
 
    })
    .catch((error) =>{
