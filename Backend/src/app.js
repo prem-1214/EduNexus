71d261faa7a9453967c17b5c9222d86a88df36ba
Yash Patel <121545728+Yash-Patel-42@users.noreply.github.com>
2025-04-27 00:15:30 +0530

    Update CORS configuration to use environment variable for frontend URI and refactor logout API call to use axios instance

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const app = express()
 const allowedOrigins = [
-  "https://edu-nexus-frontend-gray.vercel.app", // Deployed frontend
+  process.env.FRONTEND_URI, // Deployed frontend
   "http://localhost:5173", // Local frontend
 ];
 
