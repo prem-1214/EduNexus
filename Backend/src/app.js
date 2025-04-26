7fd48961399026f8cc9993a3aa24a2292e17dee6
Prem-Rajput <rajputpremkumar22@gmail.com>
2025-04-27 00:32:49 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -11,24 +11,31 @@ import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 const app = express()
-const allowedOrigins = [
-  process.env.FRONTEND_URI, // Deployed frontend
-  "http://localhost:5173", // Local frontend
-];
+
+
+app.use(cors({
+  origin : "*",
+}))
+
+// const allowedOrigins = [
+//   process.env.FRONTEND_URI, // Deployed frontend
+//   "http://localhost:5173", // Local frontend
+// ];
+
 
 // Middleware
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
 
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups")
