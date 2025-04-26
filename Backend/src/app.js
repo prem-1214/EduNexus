f2f64f25d71a95c0022fc5d6270022d46753e240
Yash Patel <121545728+Yash-Patel-42@users.noreply.github.com>
2025-04-26 22:29:46 +0530

    minor fix in app.js (for production purposes)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -31,18 +31,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../../frontend/dist')))
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
-    })
-  } else {
-    app.get('/', (req, res) => {
-      res.send('API is running...')
-    })
-  }
+app.get("/", (req, res) => {
+  res.send("API is running...")
+})
 
 // Routes
 app.use("/auth", authRouter)
