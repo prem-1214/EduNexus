de715dee987198f8c4b8f1484d01bf6ebfefc88d
Yash Patel <121545728+Yash-Patel-42@users.noreply.github.com>
2025-05-03 20:45:58 +0530

    Resolving Main.jsx for Production

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,8 +4,8 @@ import "./index.css"
 import App from "./App.jsx"
 import { GoogleOAuthProvider } from "@react-oauth/google"
 import { BrowserRouter, HashRouter } from "react-router-dom"
-import { ChatProvider } from "./context/ChatContext.jsx"
-import { UserProvider } from "./context/UserContext.jsx"
+import { ChatProvider } from "../src/context/ChatContext.jsx"
+import { UserProvider } from "../src/context/UserContext.jsx"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
